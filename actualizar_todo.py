e334e3b5407e7c99b9af4574a9d6310e3c0999a8
QuiniHub <marc.ruzafa.c@gmail.com>
2026-05-07 18:08:19 +0100

    Update actualizar_todo.py

--- actualizar_todo.py
+++ actualizar_todo.py
@@ -2,10 +2,12 @@ import os
 
 scripts = [
     "actualizar_historico_real.py",
-    "actualizar_clasificaciones.py"
+    "actualizar_clasificaciones.py",
     "actualizar_jornadas_detalle.py"
 ]
 
 for script in scripts:
     print(f"Ejecutando {script}...")
-    os.system(f"python {script}")
+    resultado = os.system(f"python {script}")
+    if resultado != 0:
+        raise SystemExit(f"Error ejecutando {script}")
